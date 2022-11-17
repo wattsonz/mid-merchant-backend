6a581f7759aba633cf3cf96f67e6c36bd8bd729e
wattson <oitimexo@gmail.com>
2022-11-17 10:00:16 +0700

    add CrossOrigin

--- src/main/java/com/midmerchant/midmerchantbackend/controller/UserController.java
+++ src/main/java/com/midmerchant/midmerchantbackend/controller/UserController.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
